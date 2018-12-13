f22f6c24f1651cb7cf97d90591600f52389849d0
jinseongjoo <evanjin8@gmail.com>
2018-12-13 09:16:53 +0900

    Auth #2 : 회원가입 (Social 로그인 연동)

--- firebast-test/public/assets/js/auth.js
+++ firebast-test/public/assets/js/auth.js
@@ -9,6 +9,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
+
 $(document).ready(function($) {
   firebase.auth().onAuthStateChanged(function(user) {
     var cu = window.location.href
@@ -58,4 +59,40 @@ function login() {
       const errorCode = error.code
       alert(errorCode)
     })
+}
+
+function facebooklogin() {
+	var user = firebase.auth().currentUser;
+	if (user) {
+		//
+	} else {
+
+		var provider = new firebase.auth.FacebookAuthProvider();
+		firebase.auth().signInWithPopup(provider).then(function (result) {
+			// This gives you a Facebook Access Token.
+			// var token = result.credential.accessToken;
+			// The signed-in user info.
+			// var user = result.user;
+			console.log("facebook connected");
+		}).catch(function (error) {
+			console.log(error);
+		});
+	}
+}
+
+function googlelogin() {
+	var user = firebase.auth().currentUser;
+
+	if (user) { //
+	} else {
+		var provider = new firebase.auth.GoogleAuthProvider();
+		provider.addScope('https://www.googleapis.com/auth/plus.login');
+		firebase.auth().signInWithPopup(provider).then(function (result) {
+			// This gives you a Google Access Token. You can use it to access the Google API. var token = result.credential.accessToken;
+			var user = result.user;
+			console.log("connected");
+		}).catch(function (error) {
+			console.log('error',error);
+		});
+	}
 }
